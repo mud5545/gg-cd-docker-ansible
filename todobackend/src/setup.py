aca3e9555fddc45508fc990baa39cd9030e87aa4
Mugdha <mud5545@rit.edu>
2019-04-21 19:53:24 -0400

    updated

--- todobackend/src/setup.py
+++ todobackend/src/setup.py
@@ -8,22 +8,22 @@ setup (
     include_package_data    = True,
     scripts                 = ["manage.py"],
     install_requires        = [
-                                "Django==1.9",
-                                "django-cors-headers==1.1.0",
-                                "django-nose==1.4.4",
-                                "djangorestframework==3.3.0",
-                                "MySQL-python==1.2.5",
-                                "packaging==16.8",
-                                "pyparsing==2.2.0",
+                                "Django>=1.9, <2.0",
+                                "django-cors-headers>=1.1.0",
+                                "django-nose>=1.4.4",
+                                "djangorestframework>=3.3.0",
+                                "MySQL-python>=1.2.5",
+                                "packaging>=16.8",
+                                "pyparsing>=2.2.0",
                                 "uwsgi>=2.0"
                             ],
     extras_require          = {
                                 "test": [
-                                            "colorama==0.3.9",
-                                            "coverage==4.3.4",
-                                            "django-nose==1.4.4",
-                                            "nose==1.3.7",
-                                            "pinocchio==0.4.2"
+                                            "colorama>=0.3.9",
+                                            "coverage>=4.3.4",
+                                            "django-nose>=1.4.4",
+                                            "nose>=1.3.7",
+                                            "pinocchio>=0.4.2"
                                         ]
                                 }
 )
